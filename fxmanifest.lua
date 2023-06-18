69a5e9aa4996974895f510261105986bd72358ba
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-06-18 10:26:46 +0200

    chore: initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+game 'gta5'
+
+version '0.0.4'
+
+client_scripts {
+	'client/enginesmoke.lua',
+	'config.lua',
+	'client/damage.lua'
+}
+
+shared_script {
+	'@es_extended/imports.lua'
+}
